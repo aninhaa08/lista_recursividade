094110156a565d9de2d058421c977c5b102c249f
EA - Ana Carolina Souto Miranda <ana_souto@educadventista.org>
2025-03-18 21:20:41 -0300

    Exercício 1

--- Parte 1.cpp
+++ Parte 1.cpp
@@ -1,1 +1,29 @@
-#include <stdio.lib>
+#include <stdio.h>
+
+int fat(int num){
+	
+	if(num == 0 || num == 1){
+		return 1;
+	}
+	
+	return num * fat(num - 1);
+}
+
+int main(){
+	int num;
+	int result;
+	
+	printf("Insira o número desejado: ");
+	scanf("%d", &num);
+	
+	if(num == 0 || num < 0) {
+		printf("\n Não é possível calcular a fatorial.");
+	}
+	else {
+		result = fat(num);
+	}
+	
+	printf("Resultado da fatorial: %d", result);
+}
+
+
