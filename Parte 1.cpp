a175e86fcda73d8c05a00dd06ad80e5eae84e5ac
EA - Ana Carolina Souto Miranda <ana_souto@educadventista.org>
2025-03-18 22:42:10 -0300

    Finalização dos exercícios 1 a 7

--- Parte 1.cpp
+++ Parte 1.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*  FATORIAL
 
@@ -23,6 +24,75 @@ int somarNum(int num){
 	}
 	
 	return num + somarNum(num - 1);
+}
+
+*/
+
+/*  FIBONACCI
+
+int termoFibonacci(int num){
+	if(num == 0){
+		return 0;
+	} else if(num == 1){
+		return 1;
+	}
+	
+	return termoFibonacci(num - 1) + termoFibonacci(num - 2);
+}
+
+*/
+
+/* POTÊNCIA
+
+int potencia(int n, int x){
+	if(n == 0){
+		return 1;
+	}
+	
+	return x * potencia(n-1, x);
+}
+
+*/
+
+/*  CONTAGEM REGRESSIVA
+
+void contagemRegressiva(int num){
+	printf("%d\n", num);
+	
+	if(num == 0){
+		printf("\nLançamento!");
+		return;
+	}
+	
+	return contagemRegressiva(num-1);
+}
+
+*/
+
+/*  SOMA DÍGITOS
+
+int somaDig(int num){
+	if(num == 0){
+		return 0;
+	}
+	
+	return (num % 10) + somaDig(num / 10);
+}
+
+*/
+
+/* INVERSÃO DE STRINGS
+
+void inverterString(char * s, int inicial, int final){
+	if(inicial >= final){
+		return;
+	}
+	
+	char t = s[inicial];
+	s[inicial] = s[final];
+	s[final] = t;
+	
+	inverterString(s, inicial + 1, final - 1);
 }
 
 */
@@ -57,6 +127,66 @@ int main(){
 	
 	int result = somarNum(quant);
 	printf("Resultado da soma: %d", result);
+	
+	*/
+	
+	/*  FIBONACCI
+	
+	int num;
+	
+	printf("Informe o termo a ser retornado de Fibonacci: ");
+	scanf("%d", &num);
+	
+	int termo = termoFibonacci(num);
+	printf("Termo da sequência: %d", termo);
+	
+	*/
+	
+	/*  POTÊNCIA
+	
+	int n;
+	int x;
+	
+	printf("Informe o número a ser n (no caso em que será n elevado a x): ");
+	scanf("%d", &n);
+	
+	printf("Informe o número a ser x (no caso em que será n elevado a x): ");
+	scanf("%d", &x);
+	
+	int result = potencia(n, x);
+	printf("Resultado da potência: %d", result);
+	
+	*/
+	
+	/*  CONTAGEM REGRESSIVA
+	
+	int num;
+	
+	printf("Informe o início da contagem regressiva: ");
+	scanf("%d", &num);
+	
+	contagemRegressiva(num);
+	
+	*/
+	
+	/*  SOMA DÍGITOS
+	
+	int num;
+	
+	printf("Informe um número: ");
+	scanf("%d", &num);
+	
+	int result = somaDig(num);
+	printf("Resultado da soma dos dígitos: %d", result);
+	
+	*/
+	
+	/*  INVERSÃO DE STRINGS
+	
+	char s[] = "recursão";
+	
+	inverterString(s, 0, strlen(s) - 1);
+	printf("%s\n", s);
 	
 	*/
 	
